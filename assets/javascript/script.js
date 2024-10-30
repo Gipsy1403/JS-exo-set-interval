5df412cd5cd3ec965cc9544b015d7bd0cf36a468
Gipsy Rouaret <gipsy.rouaret@orange.fr>
2024-10-30 16:38:03 +0100

    first commit

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,34 +1,88 @@
-// exo 1****************************
-// setTimeout(() => {
-//   const welcome = document.getElementById('welcome');
-//   welcome.textContent = "Bienvenue dans le monde du JavaScript !";
-// }, 2000);
+let todo = document.getElementById("todo");
+let finish = document.getElementById("finish");
+let text = document.querySelector(".text");
+const btn = document.getElementById("btn");
+const error=document.getElementById("error")
+let tasks=[]
 
-// exo 2 ********************************
-// let seconds = 5; 
-// const countdown = document.getElementById('countdown');
+btn.addEventListener("click", function (){
+      error.textContent=""
+      if (!text.value){
+            error.textContent="Merci de saisir une tâche avant de cliquer sur Ajouter"
+            return
+      }
 
-// const interval = setInterval(() => {
-//     if (seconds > 0) {
-//         countdown.textContent = seconds + " secondes restantes";
-//         seconds--; 
-//     } else {
-//         countdown.textContent = "Go !";
-//         clearInterval(interval); 
-//     }
-// }, 1000); 
+      if (tasks.includes(text.value)){
+            error.textContent="Cette tâche existe déjà !"
+            return
+     }
+      tasks.push(text.value)
+      let listItem=document.createElement("li")
+      listItem.textContent=text.value
+      todo.appendChild(listItem)
+      text.value=""
 
-// exo 3 *************************************
-// const blink=document.getElementById("blink");
+      const modifBtn=document.createElement("button")
+      listItem.appendChild(modifBtn)
+      modifBtn.textContent="Modifier"
+      modifBtn.style.color= "white";
+      modifBtn.style.background = "blue";
+      modifBtn.style.padding = "0 10px";
+      modifBtn.style.margin = "0 10px";
 
-// setInterval(() => {
-//  blink.style.visibility = (blink.style.visibility == "hidden" ? "" : "hidden");
-// },500);
+      const completeBtn=document.createElement("button")
+      listItem.appendChild(completeBtn)
+      completeBtn.textContent="Terminée"
+      completeBtn.style.color= "white";
+      completeBtn.style.background = "green";
+      completeBtn.style.padding = "0 10px";
+      completeBtn.style.margin = "0 10px";
+     
+      const dellBtn=document.createElement("button")
+      listItem.appendChild(dellBtn)
+      dellBtn.textContent="Supprimer"
+      dellBtn.style.color= "white";
+      dellBtn.style.background = "red";
+      dellBtn.style.padding = "0 10px";
+      dellBtn.style.margin = "0 10px";
 
-// exo4 ****************************************
-let clock=document.getElementById("clock");
-let h= new Date ();
+      modifBtn.addEventListener("click", function() {
+            todo.appendChild(listItem)
+            modifBtn.textContent
 
-setInterval(() => {
-  h.getHours() + ":" + h.getMinutes() + ":" + h.getSeconds();
-}, 1000);
+
+      })
+
+      completeBtn.addEventListener("click", function() {
+            todo.appendChild(listItem)
+            completeBtn.style.visibility="hidden"
+      
+      })
+
+      dellBtn.addEventListener("click", function (){
+            todo.appendChild(listItem)
+            listItem.remove()
+            
+      })
+
+      modifBtn.addEventListener("click", function() {
+            finish.appendChild(listItem)
+            modifBtn.style.visibility="hidden"
+
+      })
+
+      completeBtn.addEventListener("click", function() {
+            finish.appendChild(listItem)
+            completeBtn.textContent="En cours"
+           
+      
+      })
+
+      dellBtn.addEventListener("click", function (){
+            finish.appendChild(listItem)
+            listItem.remove()
+            
+      })
+
+
+})
